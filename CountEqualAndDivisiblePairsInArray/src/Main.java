8e7ae0d32125556973f4315c32834af03f655230
Anna Nebuko <Anna_Nebuko@epam.com>
2022-07-04 10:25:14 +0300

    Count Equal and Divisible Pairs

---
+++ CountEqualAndDivisiblePairsInArray/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    int [] arr = {3,1,2,2,2,1,3};
+    int k = 2;
+
+    System.out.println(Solution.countPairs(arr, k));
+  }
+}
+
+class Solution {
+  public static int countPairs(int[] nums, int k) {
+    int counter = 0;
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] == nums[j] &&
+            i * j % k == 0 &&
+            i < j) {
+          counter++;
+        }
+      }
+    }
+    return counter;
+  }
+}
