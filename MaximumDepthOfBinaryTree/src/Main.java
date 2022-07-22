0328c4afac2df46b45850683a6e48cd7730f620a
Anna Nebuko <Anna_Nebuko@epam.com>
2022-07-22 11:35:22 +0300

    Binary tree's maximum depth

--- MaximumDepthOfBinaryTree/src/Main.java
+++ MaximumDepthOfBinaryTree/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!!");
+    TreeNode treeNode = new TreeNode(3,
+        new TreeNode(9, null, null),
+        new TreeNode(20, new TreeNode(17), new TreeNode(7)));
+
+    Solution solution = new Solution();
+    System.out.println(solution.maxDepth(treeNode));
   }
 }
